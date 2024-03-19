cdd990ec76f4026e99f3a5e7fee4b935655cd80e
William Itália Nogueira <89314780+William-italia@users.noreply.github.com>
2024-03-19 03:48:53 -0300

    Update agenda.sql

--- agenda.sql
+++ agenda.sql
@@ -8,11 +8,11 @@ CREATE TABLE contacts(
 );
 
 INSERT INTO contacts (name, phone, observations) VALUES
-('', '(13)995671223', 'Conhecido por ser um cliente regular.'),
-('', '(13)9923451245', 'Costuma fazer pedidos grandes.'),
-('', '(13)9918235678', 'Amiga próxima do dono.'),
-('', '(13)12345678', 'Novo cliente.'),
-('', '(13)98765432', 'Fornecedor confiável.'),
-('', '(13)98765432', 'cliente antigo'),
-('', '(13)98765432', 'Antigo cliente, às vezes pede grandes quantidades.');
+('Ana', '(13)995671223', 'Conhecido por ser um cliente regular.'),
+('João', '(13)9923451245', 'Costuma fazer pedidos grandes.'),
+('Maria', '(13)9918235678', 'Amiga próxima do dono.'),
+('Carlos', '(13)12345678', 'Novo cliente.'),
+('Fernanda', '(13)98765432', 'Fornecedor confiável.'),
+('Pedro', '(13)98765432', 'cliente antigo'),
+('Laura', '(13)98765432', 'Antigo cliente, às vezes pede grandes quantidades.');
 
