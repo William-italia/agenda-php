71226fcf17836b29ea5c32a122ecf545c2375a91
Chart 70 <williamitalia70@outlook.com>
2024-03-19 00:49:24 -0300

    starter

--- templates/header.php
+++ templates/header.php
@@ -8,13 +8,13 @@ include_once("config/url.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda</title>
+    <title>Agenda de Contatos</title>
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- CSS -->
-    <link rel="stylesheet" href="<?php $BASE_URL ?>css/styles.css">
+    <link rel="stylesheet" href="<?php $BASE_URL ?>assets/css/output.css">
 </head>
 <body>
-
+    
 </body>
 </html>
